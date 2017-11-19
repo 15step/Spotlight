c464a1e0566dfb2f19698cfcfa838c72e5c1574c
Michael Pszonka <mjp529@gmail.com>
2017-11-19 12:10:46 -0500

    Home page layout

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,41 @@
 import * as React from 'react';
-import { Jumbotron } from 'react-bootstrap';
+// import { Jumbotron } from 'react-bootstrap';
 
 const Home = () => {
     return(
-        <Jumbotron>
-            This is a Jumbotron!
-        </Jumbotron>
+        <div className="container">   
+            <h1>Hackathon Starter</h1>
+            <div className="row">
+                <div className="col-sm-6">
+                    <h2>Heading</h2>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, 
+                        tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem 
+                        malesuada magna mollis euismod. Donec sed odio dui.</p>
+                    <a className="btn btn-default" href="#" role="button">View Details >></a>
+                </div>
+                <div className="col-sm-6">
+                    <h2>Heading</h2>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, 
+                        tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem 
+                        malesuada magna mollis euismod. Donec sed odio dui.</p>
+                    <a className="btn btn-default" href="#" role="button">View Details >></a>
+                </div>
+                <div className="col-sm-6">
+                    <h2>Heading</h2>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, 
+                        tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem 
+                        malesuada magna mollis euismod. Donec sed odio dui.</p>
+                    <a className="btn btn-default" href="#" role="button">View Details >></a>
+                </div>
+                <div className="col-sm-6">
+                    <h2>Heading</h2>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, 
+                        tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem 
+                        malesuada magna mollis euismod. Donec sed odio dui.</p>
+                    <a className="btn btn-default" href="#" role="button">View Details >></a>
+                </div>
+            </div>
+        </div>
     );
 };
 
